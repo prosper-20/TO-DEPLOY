8b924923188245bb1f4906aa34ad2379802b0e72
prosper <edwardprosper001@gmail.com>
2023-03-20 21:14:41 +0100

    'updates'

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-xco6)qu+vhn5^buro)upchu4i)*@tpy!#5v5vd6f^uowr+p-+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','prosper-demo.herokuapp.com']
+ALLOWED_HOSTS = ['prosper-demo.herokuapp.com']
 
 
 # Application definition
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'MAIN.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-DATABASES = {     
+DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'demo',
-    'USER': 'postgres',
-    'PASSWORD': os.getenv("my_password"),
-    'HOST': 'localhost',
-    'PORT': '5432'  
-} 
-} 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# DATABASES = {     
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'demo',
+#     'USER': 'postgres',
+#     'PASSWORD': os.getenv("my_password"),
+#     'HOST': 'localhost',
+#     'PORT': '5432'  
+# } 
+# } 
 
 
 # Password validation
