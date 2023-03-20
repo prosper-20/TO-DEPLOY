5bc32614fc51fdc9cca6ee6abe15b8f58c31b6e2
prosper <edwardprosper001@gmail.com>
2023-03-20 21:11:34 +0100

    'updates'

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-xco6)qu+vhn5^buro)upchu4i)*@tpy!#5v5vd6f^uowr+p-+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://prosper-demo.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','prosper-demo.herokuapp.com']
 
 
 # Application definition
